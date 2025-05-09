46ddaa57bb70d9cae02131f899c446454f77e757
Sayan De <sayandeten@gmail.com>
2025-05-09 22:37:53 +0530

    refactor:deleted server folder

--- server/get-embedding.py
+++
@@ -1,8 +0,0 @@
-api_key = getpass.getpass('OpenAI API Key: ')
-openai.api_key = api_key
-client = OpenAI(api_key = api_key)
-
-def get_embedding(text, model="text-embedding-ada-002"):
-    text = text.replace("\n", " ")
-    response = openai.embeddings.create(input=[text], model=model)
-    return response.data[0].embedding
