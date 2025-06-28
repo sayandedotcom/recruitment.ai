a6c808f7116de56e5108b5686860822845559ead
Sayan De <sayandeten@gmail.com>
2025-06-29 02:03:45 +0530

    feat: added major api routes

--- apps/api/app/__init__.py
+++ apps/api/app/__init__.py
@@ -3,9 +3,9 @@ import os
 from app.blueprint import register_routing
 from app.db import mongo
 from app.extention import cors
-# from app.utils.logging import configure_logging
-from flask import Flask
+from app.utils.logging import configure_logging
 
+from flask import Flask  
 
 def create_app(settings_module):
     app = Flask(__name__)
@@ -16,7 +16,7 @@ def create_app(settings_module):
     cors.init_app(app, supports_credentials="true", resources={r"*": {"origins": "*"}})
 
     # Logging configuration
-    # configure_logging(app)
+    configure_logging(app)
 
     # Register Blueprint
     register_routing(app)
--- apps/api/app/controllers/example.py
+++
@@ -1,9 +0,0 @@
-
-from flask_smorest import Blueprint
-
-blp = Blueprint("Job", __name__, description="Job API")
-
-
-@blp.route("/job")
-def hello_world():
-    return "<p>Hello, World!</p>"
